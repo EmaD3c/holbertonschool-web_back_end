a1559481018c577f67fabf424544e434457d15e2
EmaD3c <>
2024-11-11 17:51:23 +0100

    add comment and ident

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   // le code renvoie la liste des objets de employeesList & le nombre de clés
     return {
-      // allEmployees stocke les objets stocké dans employeesList
+      // all Employees stocke les objets stocké dans employeesList
       allEmployees: { ...employeesList },
       getNumberOfDepartments() {
         // retourne le nombre de key dans employeesList
