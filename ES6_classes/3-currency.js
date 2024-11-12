34b60f10783fe3887d16641dfdb9dade92c7ff2c
EmaD3c <>
2024-11-12 19:30:23 +0100

    ADD ESLINT STYLE

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,6 +1,5 @@
 export default class Currency {
   constructor(code, name) {
-    // Validate that 'code' and 'name' are strings
     if (typeof code !== 'string') {
       throw new TypeError('Code must be a string');
     }
@@ -8,13 +7,16 @@ export default class Currency {
       throw new TypeError('Name must be a string');
     }
 
-    // Assign private properties
+    // Assign private properties with ESLint rule disabled for these lines
+    // eslint-disable-next-line no-underscore-dangle
     this._code = code;
+    // eslint-disable-next-line no-underscore-dangle
     this._name = name;
   }
 
   // Getter and setter for 'code' attribute
   get code() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._code;
   }
 
@@ -22,11 +24,13 @@ export default class Currency {
     if (typeof newCode !== 'string') {
       throw new TypeError('Code must be a string');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._code = newCode;
   }
 
   // Getter and setter for 'name' attribute
   get name() {
+    // eslint-disable-next-line no-underscore-dangle
     return this._name;
   }
 
@@ -34,11 +38,13 @@ export default class Currency {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
+    // eslint-disable-next-line no-underscore-dangle
     this._name = newName;
   }
 
   // Method to display currency in "name (code)" format
   displayFullCurrency() {
+    // eslint-disable-next-line no-underscore-dangle
     return `${this._name} (${this._code})`;
   }
 }
