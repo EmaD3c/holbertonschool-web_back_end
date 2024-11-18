2a6d8884f9a6882705963a0cc08a0c3840ebc852
EmaD3c <>
2024-11-18 16:16:17 +0100

    create function that map student id

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+export default function getListStudents(objet_student) {
+  // verrifie que l'objet est un tableau
+  if (!Array.isArray(objet_student)){
+    return[]
+  }
+
+  // map cree un tableau avec les id des students
+  return objet_student.map(student => student.id);
+}
