7612cb531377dc6b28164adafb0ec640e69e75f7
EmaD3c <>
2024-11-18 18:36:56 +0100

    add a filter for student cities

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,4 @@
-export default function getStudentsByLocation() {
-  return [
-    
-  ]
+export default function getStudentsByLocation(objetstudent, city) {
+  // Utilise filter pour sélectionner les étudiants de la ville spécifiée
+  return objetstudent.filter((student) => student.location === city);
 }
