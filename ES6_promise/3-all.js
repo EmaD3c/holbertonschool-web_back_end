438ec7ba02a21fa9501a77e8a5cabb0a48b1ab00
EmaD3c <>
2024-11-21 20:31:25 +0100

    add file that Handle multiple successful promises

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,16 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      // Extrait les valeurs des objets résolus
+      const { body } = results[0];
+      const { firstName, lastName } = results[1];
+
+      // Afficher le bon format des valeurs
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
