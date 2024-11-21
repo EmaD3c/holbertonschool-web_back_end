98e4dee022b1f76cd521aed9cefec480bf241963
EmaD3c <>
2024-11-21 23:03:56 +0100

    try to delete the then and map  part

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,16 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  // Utilisation de Promise.allSettled pour attendre que toutes les promesses se terminent
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)
-  ])
-  .then((results) =>
-    // Utilisation de map pour structurer chaque résultat sous la forme { status, value }.
-    results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }))
-  );
+  const result = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return result.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : `Error: ${result.reason.message}`,
+  }));
 }
