077f0748ab7c81151eec8fd0cd91080129b29960
EmaD3c <>
2024-11-22 01:32:32 +0100

    add the result to queue if  an eeror appear

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,10 +1,14 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+  
   try {
-    queue.push(mathFunction);
+    const result = mathFunction();
+    queue.push(result);
   } catch (error) {
     queue.push(`Error: ${error.message}`);
   }
+  
   queue.push("Guardrail was processed");
+  
   return queue;
 }
