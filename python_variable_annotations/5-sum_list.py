71fa603b4fc039e84a5f133b1729de856acca06e
EmaD3c <>
2024-12-03 17:06:23 +0100

    add all function and file comment

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+
 
 def sum_list(input_list: list[float]) -> float:
+    """
+    returns their sum as a float.
+    """
     return sum(input_list)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
+"""
+Complex types - string and int/float to tuple
+"""
 from typing import List, Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Converts a string and a number (int or float) into a tuple.
+
+    Args:
+        k (str): A string key.
+        v (Union[int, float]): A number to be squared.
+
+    Returns:
+        Tuple[str, float]: tuple containing the string and the square number.
+    """
     return k, (v ** 2)
