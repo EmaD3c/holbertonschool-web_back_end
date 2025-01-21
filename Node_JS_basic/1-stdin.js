66f22dc8ce13d0f2553eaf43a95794b2cc3973a9
Ema Decot <emadecot03@gmail.com>
2025-01-21 16:40:54 +0100

    add package lock json

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,9 +2,10 @@ console.log("Welcome to Holberton School, what is your name?");
   
 process.stdin.on('data', data => { 
   console.log(`Your name is: ${data.toString()}`);
+  process.stdin.end();
+});
 
 // fin du programme
 process.stdin.on('end', () => {
   console.log("This important software is now closing\n");
-});
 });
