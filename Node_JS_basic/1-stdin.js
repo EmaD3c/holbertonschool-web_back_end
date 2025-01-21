438f4b84268612de1af998db8706e7c958858e0f
Ema Decot <emadecot03@gmail.com>
2025-01-21 18:15:39 +0100

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,10 @@
-console.log("Welcome to Holberton School, what is your name?");
-  
-process.stdin.on('data', data => { 
-  console.log(`Your name is: ${data.toString()}`);
-  process.stdin.end();
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', data => {
+  console.log(`Your name is: ${data.toString().trim()}`);
 });
 
 // fin du programme
 process.stdin.on('end', () => {
-  console.log("This important software is now closing\n");
+  console.log('This important software is now closing\n');
 });
