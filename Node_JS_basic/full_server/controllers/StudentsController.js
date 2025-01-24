25d4a63d0facf33d3bfcdefbbe9486bb8942825e
Ema Decot <emadecot03@gmail.com>
2025-01-24 20:31:36 +0100

    add lint

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,8 +4,8 @@ export default class StudentsController {
   static async getAllStudents(req, res) {
     try {
       const studentsData = await readDatabase(process.argv[2]);
-      const fields = Object.keys(studentsData).sort((a, b) =>
-      a.toLowerCase().localeCompare(b.toLowerCase()),
+      const fields = Object.keys(studentsData).sort(
+        (a, b) => a.toLowerCase().localeCompare(b.toLowerCase()),
       );
 
       let result = 'This is the list of our students\n';
