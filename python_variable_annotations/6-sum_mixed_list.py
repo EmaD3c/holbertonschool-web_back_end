76177227afafee837028c0ac1467b052ed5c4642
Ema Decot <emadecot03@gmail.com>
2025-04-24 04:11:41 +0200

    delete a space

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ sum_mixed_list
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List [Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """
     sum_mixed_list
     """
