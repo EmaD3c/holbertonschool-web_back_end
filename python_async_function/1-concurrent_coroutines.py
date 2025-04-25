60c260deb615406e52a5f0758c1f21c1ae9cf545
Ema Decot <emadecot03@gmail.com>
2025-04-25 18:51:31 +0200

    add pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,9 +2,10 @@
 """
 mettre un delais aleatoire entre 0 et max delay
 """
-wait_random = __import__("0-basic_async_syntax").wait_random
-import asyncio, random
+import asyncio
+import random
 from typing import List
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
@@ -12,11 +13,11 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     async
     """
     delays = []  # cree une list vide
-    
-    for i in range(n):  # ajouter les resultat 
+
+    for i in range(n):  # ajouter les resultat
         delay = await wait_random(max_delay)
         delays.append(delay)
-    
+
     # trier les resultats
     delays.sort()
     return delays
