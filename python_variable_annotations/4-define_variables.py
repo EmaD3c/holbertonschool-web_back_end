6985564af2fc5e93fdb749719418be57d66d9fd9
Ema Decot <emadecot03@gmail.com>
2025-04-25 23:59:21 +0200

    add t4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 """
-documented
+mesure le temps d'exécution moyen de plusieurs coroutines
+asynchrones exécutées en parallèle
 """
+import asyncio
+from typing import List
+task_wait_random = __import__("3-tasks").task_wait_random
 
 
-a: int = 1
-pi: float = 3.14
-i_understand_annotations: bool = True
-school: str = "Holberton"
+async def task_wait_n(n: int,max_delay: int) -> List[float]:
+    """
+    async
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    results = []
+    
+    for completed_task in asyncio.as_completed(tasks):
+        delay = await completed_task
+
+        results.append(delay)
+
+    return sorted(results)
