485894ebb82b5754a2a060f2acfa129ff302687e
Ema Decot <emadecot03@gmail.com>
2025-04-29 16:52:41 +0200

    add t0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+coroutine will loop 10 times,
+each time asynchronously wait 1 second,
+then yield a random number between 0 and 10.
+"""
+import random
+import asyncio
+from typing import Generator
+"""
+async
+"""
+
+
+async def async_generator() -> Generator[float, None, None]:
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
