854c2175d533d308a738a16b3bd93e5ca9cb3c41
Ema Decot <emadecot03@gmail.com>
2025-04-30 04:02:55 +0200

    add t3

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+async
+"""
+import time
+import asyncio
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    xcute async comprehension 4fois
+    et use asyncio gater en parallel
+    mesure du temps total a la fin
+    """
+    first = time.time()
+    await asyncio.gather(*(async_comprehension() for x in range(4)))
+    last = time.time()
+    return first - last
